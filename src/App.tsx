985db2cc2985eedb0072f7fdc5765aa90889fac0
Govish <9davidmuia@gmail.com>
2021-07-23 10:36:06 +0300

    Navigation Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import React,{useEffect, Suspense} from 'react';
 import logo from './logo.svg';
+import {AppBar ,Toolbar, IconButton,Typography, Button} from '@material-ui/core';
+import { Menu , ShoppingBasket, ShoppingCart} from '@material-ui/icons';
 import './css/main.css'; 
+import { useStyles } from './st';
 // import {add} from './components/math';
 
 function App() {
+  const classes = useStyles();
   useEffect(() => { 
    
      
   }, [])
   return (
      <div className='main'>
+       <div className='navigation'>
+         <div className='logo '>logos</div>
+
+         <div className='navi '>
+           <ul>
+             <li>Home</li>
+             <li>Shop</li>
+             <li>Blog</li>
+             <li>Contact</li>
+           </ul>
+         </div>
+         <div className='cartIco '>
+           <ul>
+             <li>$0.00</li> 
+             <li><ShoppingCart /></li>
+           </ul>
+         </div>
+      </div>
 
      </div>
   );
