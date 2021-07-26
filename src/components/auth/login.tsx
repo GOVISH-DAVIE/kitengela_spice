72638dbe76e4ec5b8281edc79961a75f34c62550
Govish <9davidmuia@gmail.com>
2021-07-26 17:04:39 +0300

    clean up

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             // background:'red',
-            width: '50%',
+            width: '70%',
         },
         tfiled: {
             margin: theme.spacing(1),
--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
+
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
+import CardContent from '@material-ui/core/CardContent'; 
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { red } from '@material-ui/core/colors'; 
 import { ShoppingCart, SettingsOverscan } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
