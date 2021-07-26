ffdcd3c01ad35c1cdc0d302bb3cae83c1efac05d
Govish <9davidmuia@gmail.com>
2021-07-26 21:49:56 +0300

    Awesome

---
+++ src/components/viewProduct/price.tsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Typography, IconButton, CardContent, CardMedia, CardHeader, Card } from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import { ShoppingCart, SettingsOverscan } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '23vw',
+            height: '65vh',
+            borderRadius: '20px',
+            float: 'left',
+            marginLeft: '.55vw',
+            // marginBottom:'5vh'
+            background: 'rgba(255, 255, 255, .45)',
+            backdropFilter: "blur(50px)",
+
+
+        },
+        media: {
+            height: 0,
+            paddingTop: '65.25%', // 16:9
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+        avatar: {
+            backgroundColor: red[500],
+        },
+    }),
+);
+
+
+export const PriceSection = () => {
+    const classes = useStyles();
+
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <IconButton aria-label="settings">
+                        <SettingsOverscan />
+                    </IconButton>
+                    //   <Avatar aria-label="recipe" className={classes.avatar}>
+                    //     R
+                    //   </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <ShoppingCart />
+                    </IconButton>
+                }
+            />
+            <CardMedia
+                className={classes.media}
+                image="https://cdn-images.farfetch-contents.com/14/64/73/36/14647336_22738129_480.jpg"
+                title="Paella dish"
+            />
+            <CardContent>
+                <Typography style={{
+                    textAlign: 'center'
+                }} variant="h5" color="textPrimary" component="h4" > Product Name
+                </Typography>
+                <br />
+                <Typography style={{
+                    textAlign: 'center'
+                }} variant="h5" color="textPrimary" component="p" >$20.0
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
+
+
+
+const moblieCard = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '46vw',
+            height: '45vh',
+            borderRadius: '20px',
+            float: 'left',
+            marginLeft: '1vw',
+
+            marginBottom: '5vh',
+            background: 'rgba(255, 255, 255, .45)',
+            backdropFilter: "blur(50px)",
+
+
+        },
+        media: {
+            height: 0,
+            paddingTop: '65.25%', // 16:9
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+        avatar: {
+            backgroundColor: red[500],
+        },
+    }),
+);
+
+export function MobileCard() {
+    const classes = moblieCard();
+
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <IconButton aria-label="settings">
+                        <SettingsOverscan />
+                    </IconButton>
+                    //   <Avatar aria-label="recipe" className={classes.avatar}>
+                    //     R
+                    //   </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <ShoppingCart />
+                    </IconButton>
+                }
+            />
+            <CardMedia
+                className={classes.media}
+                image="https://cdn-images.farfetch-contents.com/14/64/73/36/14647336_22738129_480.jpg"
+                title="Paella dish"
+            />
+            <CardContent>
+                <Typography style={{
+                    textAlign: 'center'
+                }} variant="h5" color="textPrimary" component="h4" > Product Name
+                </Typography>
+                <br />
+                <Typography style={{
+                    textAlign: 'center'
+                }} variant="h5" color="textPrimary" component="p" >$20.0
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
--- src/components/viewProduct/viewProduct.tsx
+++ src/components/viewProduct/viewProduct.tsx
@@ -1,1 +1,12 @@
-export   const ViewProduct = () =><>view Products</>
+
+import {PriceSection} from './price'
+
+
+export const ViewProduct = () => <div className='ViewProduct'>
+
+    <div className='payment'>
+       <PriceSection />
+
+
+    </div>
+</div>
