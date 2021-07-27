6f43c146da049e9f37ef63d01cb10711af2cb93c
Govish <9davidmuia@gmail.com>
2021-07-27 13:04:32 +0300

    Alot

--- src/components/viewProduct/productView.tsx
+++ src/components/viewProduct/productView.tsx
@@ -6,11 +6,20 @@ interface ProductViewInterface {
 
 export const ProductView: React.FC<ProductViewInterface> = ({ img }) => {
     return (<div className="viewProductDetails">
+         
         <div className="mainImage">
             <img alt={img} src={img} />
         </div>
         <div className="changeImg">
-            <div className="nextImg"></div>
+            <div className="nextImg">
+                <img alt={img} src={img} />
+            </div>
+            <div className="nextImg">
+                <img alt={img} src={img} />
+            </div>
+            <div className="nextImg">
+                <img alt={img} src={img} />
+            </div>
 
         </div>
     </div>)
