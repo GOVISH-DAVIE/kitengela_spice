5dfe488afffc3463d1debac231b0d64aa0fe261e
Govish <9davidmuia@gmail.com>
2021-10-24 17:57:28 +0300

    Product Card

--- src/components/home/cards.tsx
+++ src/components/home/cards.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { makeStyles, } from '@mui/styles';
-import { Typography, IconButton, CardContent, CardMedia, CardHeader, Card, CardActions, Button } from '@mui/material';
+import { Typography, IconButton, CardContent, CardMedia, CardHeader, Card, CardActions, Button, Fab } from '@mui/material';
 import { red } from '@mui/material/colors';
-import { ShoppingCart, SettingsOverscan } from '@mui/icons-material';
+import { ShoppingCart, SettingsOverscan, AddShoppingCart } from '@mui/icons-material';
 
 
 
 const style = makeStyles({
     card: {
-        background:'rgba(255, 255, 255, .8)'
+        background: 'rgba(255, 255, 255, .8)'
     }
 })
 export default function ProductCard() {
@@ -16,7 +16,7 @@ export default function ProductCard() {
     return (
         <Card className={classes.card} >
             <CardMedia
-                component="img" 
+                component="img"
                 image="https://themegrilldemos.com/estore/wp-content/uploads/sites/50/2016/02/tie-250x180.jpg"
                 alt="Product Name"
             />
@@ -25,12 +25,14 @@ export default function ProductCard() {
                     Lizard
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                   $20 </Typography>
+                    $20 </Typography>
             </CardContent>
             <CardActions>
-                
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Fab variant="extended">
+                    <AddShoppingCart sx={{ mr: 1 }} />
+                    Navigate
+                </Fab>
+ 
             </CardActions>
         </Card>
     );
